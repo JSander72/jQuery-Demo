ef7bd06f8b49e2db7a916967b69d7523b8971c47
JSander72 <jamesearlsanders2@outlook.com>
2023-05-10 21:19:58 -0400

    second commit

--- dom.js
+++ dom.js
@@ -7,4 +7,10 @@ $('document').ready(function(){
   $('body').append('<p>I am a paragraph with text!</p>');
   $('h1').css('border','3px dotted green');
   $('.button-class').css('border','4px solid yellow');
+
+  var newText = $("<p>");
+
+  newText.append("<h1>I just replaced something</h1>");
+  $("#textbox").html(newText);
+
 });
